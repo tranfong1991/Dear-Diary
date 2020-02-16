c6ec73cfb23402e1caefced4347a85595a5a536c
Andy Tran <tran_fong1991@yahoo.com>
2020-02-16 01:27:33 -0800

    add order for search result

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -98,7 +98,7 @@ class EntriesController < ApplicationController
 
     def search
         query = params[:query].downcase.strip
-        @entries = Entry.select("created_at", "content", "id").where("user_id = ? AND lower(content) LIKE ?", current_user.id, "%#{query}%")
+        @entries = Entry.select("created_at", "content", "id").where("user_id = ? AND lower(content) LIKE ?", current_user.id, "%#{query}%").order(created_at: :desc)
     end
 
     def destroy
