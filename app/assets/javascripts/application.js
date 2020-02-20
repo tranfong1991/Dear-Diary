e7b366728fdd611e3abe749023c8273003b9baab
Andy Tran <tran_fong1991@yahoo.com>
2020-02-19 22:52:41 -0800

    add spinner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,22 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$( document ).ready(function() {
+    // hide spinner
+    $(".spinner").hide();
+  
+    // show spinner on AJAX start
+    $(document).ajaxStart(function(){
+        $(".spinner").show();
+    });
+  
+    // hide spinner on AJAX stop
+    $(document).ajaxStop(function(){
+        $(".spinner").hide();
+    });
+
+    $(".modal").on("hidden.bs.modal", function(){
+        $("#diary-date, #diary-content").html("");
+    });
+});
